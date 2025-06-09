2d3c390c2ecb9c6518612aaf8f521fa2c5f1d658
frhnaazani <nurulfarhanaana.azani@gmail.com>
2025-06-09 23:54:47 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,30 +9,23 @@
     const keyword = searchBox.value.toLowerCase();
 
     fetch("events.json")
-        .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
-        })
         .then(events => {
         const filtered = events.filter(event =>
             event.title.toLowerCase().includes(keyword)
         );
 
-        resultsList.innerHTML = "";
+        resultsList.innerHTML = ""; //clear old result
 
         if (filtered.length > 0) {
             resultsCard.style.display = "block"; // Show results
-            filtered.forEach(event => {
-            const li = document.createElement("li");
-            li.innerHTML = `<strong>${event.title}</strong><br>${event.date} | ${event.venue}`;
+            for (let i = 0; i < filtered.length; i++) {
+                let event = filtered[i];
+                let li = document.createElement("li");
+            li.innerHTML = "<strong>" + event.title + "</strong><br>" + event.date + " | " + event.venue + " | " + event.organiser;
             resultsList.appendChild(li);
-            });
-        } else {
-            resultsList.innerHTML = "<li>No events found.</li>";
+          }
         }
-        })
+            
         .catch(error => {
         resultsCard.style.display = "block";
         resultsList.innerHTML = "<li>Error loading events.</li>";
