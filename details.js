e9aa89f9735bee78d403e98838c1cdb0cfe6b9aa
Snurin1120 <nurinizzati1120@gmail.com>
2025-06-09 23:14:16 +0800

    Add files via upload

---
+++ details.js
@@ -0,0 +1,278 @@
+    const events = {
+  murattal: {
+    title: "MURATTAL 7 TARANNUM",
+    date: "18 May 2025",
+    time:"9.00 AM - 5.00PM",
+    venue: "Masjid CFS IIUM",
+    organiser: "I-MASJID and IQRA'",
+    image: "images/murattal7.jpg",
+    contact: "Br. Adib: 019-305 3320, Sr. Syazanin: 016-222 5872, Br. Ziqry: 011-58893531",
+    note: "-"
+    
+  },
+  dakwah:{
+    title: "DAKWAH TRAIL",
+    date: "25 May 2025",
+    venue: "Pelindung Hill",
+    organiser: "I-MASJID & ENC",
+    image: "images/Dakwah Trail.jpg",
+    contact: "Sr. Hidayah (i-Masjid): 018-400 0710,Br. Adam (i-Masjid): 017-718 0868,Sr. Syuhada (ENC): 012-259 3400,Br. Ammar (ENC): 017-273 6906",
+    note: "Fee: RM15   "
+  },
+  qiam: {
+    title: "QIAMULLAIL",
+    date: "9 May 2025",
+    venue: "Masjid CFS IIUM",
+    organiser: "I-MASJID",
+    image: "images/Qiamullail.jpg",
+    contact: "019-3053320 (Br Adib)",
+    note: "- "
+  },
+  hr: {
+    title: "YOUR PATH, YOUR RULE",
+    date: "22 April 2025",
+    venue: " Al-Khawarizmi Hall",
+    organiser: "SCC X PEERS",
+    image: "images/HarithR.jpg",
+    contact: "Sr Aliah: 019-270 4860, 018-474 8313  (Sr Wafa)",
+    note: "-"
+  },
+  careerExhib: {
+    title: "CAREER EXHIBITION",
+    date: "21-22 April 2025",
+    venue: "Multipurpose Room",
+    organiser:"MEDCY X AMETHYSS X INTECHSS X ENGENIUS X MELEX",
+    image: "images/CareerExhibition.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  purpleWalk: {
+    title: "PURPLE WALK",
+    date: "9 May 2025",
+    venue: "Masjid CFS IIUM",
+    organiser: "PEERS",
+    image: "images/Purple walk.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  citraEnd: {
+    title: "CITRA: THE END",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "CITRA",
+    image: "images/Citra The End.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  citraditional: {
+    title: "CITRADITIONAL",
+    date: "2 February 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "CITRA",
+    image: "images/citraditional.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  ladiesNight: {
+    title: "LADIES' NIGHT",
+    date: "25 March 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "CITRA",
+    image: "images/Ladies Night.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+ 
+  // Add more...
+   MasyitahExplorer: {
+    title: "MASYITAH THE EXPLORER",
+    date: "18 May 2025",
+    time:"9.00 AM - 5.00PM",
+    venue: "MPR Mahallah Umamah",
+    organiser: "HRC HISBAH MASYITAH",
+    image: "images/MasyitahExplorer.jpg",
+    contact: "019-3053320 (Sr Nurin)",
+    note: "-"
+    
+  },
+  Bibimplant:{
+    title: "BIBIMPLANT",
+    date: "25 May 2025",
+    venue: "HRC Hisbah Asma' Abu Bakar",
+    organiser: "I-MASJID & ENC",
+    image: "images/Bibimplant.jpg",
+    contact: "019-3053320 (Sr Nurin)",
+    note: "Fee: RM15   "
+  },
+  KhatCompetition: {
+    title: "KHAT COMPETITION",
+    date: "9 May 2025",
+    venue: "MRC Mahallah Umamah",
+    organiser: "MPR Mahallah Umamah",
+    image: "images/KhatCompetition.jpg",
+    contact: "019-3053320 (Sr Nurin)",
+    note: "- "
+  },
+  Futsal: {
+    title: "FUTSAL SHOWDOWN",
+    date: "22 April 2025",
+    venue: " Stadium cfs iium",
+    organiser: "MRC MAHALLAH AISYAH",
+    image: "images/Futsal.jpg",
+    contact: "Sr Aliah: 019-270 4860, 018-474 8313  (Sr Wafa)",
+    note: "-"
+  },
+  AishahMakeOver: {
+    title: "AISHAH MAKE OVER",
+    date: "21-22 April 2025",
+    venue: "Multipurpose Room",
+    organiser:"MRC MAHALLAH AISYAH",
+    image: "images/AishahMakeOver.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  Basketball: {
+    title: "BASKETBALL TOURNAMENT",
+    date: "9 May 2025",
+    venue: "Basketball court iium",
+    organiser: "MRC MAHALLAH AISYAH",
+    image: "images/Basketball.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  BeautyDeen: {
+    title: "BEAUTY WITH DEEN",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "MRC MAHALLAH FATIMA",
+    image: "images/BeautyDeen.jpg",
+    contact: "019-3053320 (Sr Irdina)",
+    note: "-"
+  },
+  FatimaFiesta: {
+    title: "FATIMA D'FIESTA",
+    date: "2 February 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "MRC MAHALLAH FATIMA",
+    image: "images/FatimaFiesta.jpg",
+    contact: "019-3053320 (Sr Lutfiyah)",
+    note: "-"
+  },
+  LoveSpikes: {
+    title: "LOVE YOUR SPIKES",
+    date: "25 March 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "MRC MAHALLAH FATIMA",
+    image: "images/LoveSpikes.jpg",
+    contact: "019-3053320 (Sr Farhana)",
+    note: "-"
+  },
+  //Society
+  roboticCompetition: {
+    title: "ROBOTIC WORKSHOP",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "IIUM ROBOTIC x INTECHSS x ENGENIUS",
+    image: "images/robotic competition.png",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  codeOfWarsPoster: {
+    title: "CODE OF WARS",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "INTECHSS",
+    image: "images/code of wars poster.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  mentorMentee: {
+    title: "MENTOR MENTEE",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "INTECHSS",
+    image: "images/mentor mentee 1.jpg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+ medcyplaylist: {
+    title: "MEDCY PLAYLIST",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "MEDCY",
+    image: "images/medcyplaylist.jpeg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  codeRedmedcy: {
+    title: "MEDCY RESPONSE",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "MEDCY x RCC",
+    image: "images/code red medcy poster.png",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  medcyplaylist: {
+    title: "MEDCY PLAYLIST",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "MEDCY",
+    image: "images/medcyplaylist.jpeg",
+    contact: "019-3053320 (Br Adib),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  lexTheatre: {
+    title: "LEX-THEATRE",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "CITRA",
+    image: "images/lexTheatre.jpg",
+    contact: "019-3053620 (Br Aiman),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  LEXFLIX: {
+    title: "LEX-FLIX",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "CITRA",
+    image: "images/LEX-FLIX.jpg",
+    contact: "019-3053620 (Br Aiman),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+  lextheatre: {
+    title: "LEX-TALK",
+    date: "17 May 2025",
+    venue: "Khawarizmi Hall",
+    organiser: "CITRA",
+    image: "images/lextheatre ticket.jpg",
+    contact: "019-3053620 (Br Aiman),  011-58893531 (Br Ziqry)",
+    note: "-"
+  },
+};
+    
+window.onload = function () {
+  const id = localStorage.getItem('selectedEvent');
+  const event = events[id];
+  const contactList = event.contact.split(',').map(contact => `<li>${contact.trim()}</li>`).join('');
+
+  if (event) {
+    document.querySelector(".poster img").src = event.image;
+    document.title = event.title;
+    const details = document.querySelectorAll(".details li");
+    details[0].innerHTML = `<strong>DATE:</strong> ${event.date}`;
+    details[1].innerHTML = `<strong>TIME:</strong> ${event.time}`; // if available
+    details[2].innerHTML = `<strong>VENUE:</strong> ${event.venue}`;
+    details[3].innerHTML = `<strong>ORGANISER:</strong> ${event.organiser}`;
+    // Format contact list as bullet points
+    const contactListHTML = event.contact
+      .split(',')
+      .map(contact => `<li>${contact.trim()}</li>`)
+      .join('');
+    details[4].innerHTML = `<strong>CONTACT:</strong><ul style="margin: 5px 0 0 20px;">${contactListHTML}</ul>`;
+    details[5].innerHTML = `<strong>NOTE:</strong> ${event.note}`;
+  } else {
+    document.body.innerHTML = "<h2>No event selected.</h2>";
+  }
+};
+
